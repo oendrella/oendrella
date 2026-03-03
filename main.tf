32d5fec874abf27faa0bd972386658044b56289b
oendrella <134675164+oendrella@users.noreply.github.com>
2026-03-03 23:08:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,6 @@ resource "aws_security_group" "allow_http_ssh" {
 resource "aws_instance" "test" {
     ami = "ami-0f3caa1cf4417e51b"
     instance_type = "t3.micro"
-    security_groups = "sg-0b2000da3b208432b"
     associate_public_ip_address = true
     key_name                    = aws_key_pair.deployer.key_name
     security_groups             = [aws_security_group.allow_http_ssh.name]
@@ -65,3 +64,4 @@ output "public_ip" {
   value = aws_instance.test.public_ip
   description = "Public IP of the Python application server"
 }
+
